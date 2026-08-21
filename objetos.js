7407f55fdba58633e2829ec852984b253c6646b0
kethllynsantos <kethllyn.santos@escola.pr.gov.br>
2026-08-21 11:32:20 -0300

    nnnnnnnnnnnnnnnnnnnnnn

--- objetos.js
+++ objetos.js
@@ -1,3 +1,5 @@
 const listaCPFs = [111111, 22222, 3333];
 
- const informacoesPessoa = [ nome, " jose"]
+ const informacoesPessoa = [ nome, " jose" ,"idade" , 32 , "CPF" , "111122223333"];
+
+ console.log (informacoesPessoal [1]);
